b3c8934105a8bb1d34a93fc7f7f17cee25b886c7
Siarhei Buntsevich <sebu@gurtam.com>
2018-10-18 10:40:14 +0300

    publish: v.1.0.4
    generated from commit 3256415216412893243937ac84db1eede8d79dc6

--- precache-manifest.5586bbd4bcb8565fbc831ba944c6bfa8.js
+++ precache-manifest.5586bbd4bcb8565fbc831ba944c6bfa8.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "cf6613d1adf490972c557a8e318e0868",
-    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.cf6613d1.woff"
+    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.8c2ade50.woff"
   },
   {
     "revision": "a9ff913a3c17b881b663",
@@ -12,32 +12,32 @@ self.__precacheManifest = [
     "url": "js/65f049ea.6d37374f.js"
   },
   {
-    "revision": "e8e7a69f36941e4205dbd10ff20da23d",
-    "url": "statics/mqttboard.png"
-  },
-  {
-    "revision": "f530119605145068262e",
-    "url": "js/92367fdc.9956d4b3.js"
-  },
-  {
     "revision": "81463c75bc15a1879fcd66065271a2f4",
     "url": "statics/icons/ms-icon-310x310.png"
   },
   {
-    "revision": "804957339718e5125147",
-    "url": "js/app.8a5b083f.js"
+    "revision": "c4903bb35a3504159d00",
+    "url": "js/92367fdc.6720bf4c.js"
+  },
+  {
+    "revision": "e8e7a69f36941e4205dbd10ff20da23d",
+    "url": "statics/mqttboard.png"
+  },
+  {
+    "revision": "4411d3746195abe845ae",
+    "url": "js/app.78eaee2f.js"
   },
   {
     "revision": "0e1ec93cb13fb419d792",
     "url": "js/vendor.baa35cbd.js"
   },
   {
-    "revision": "672e6d5974ed05010533442279ac8703",
-    "url": "statics/icons/icon-192x192.png"
-  },
-  {
     "revision": "3f98b5073b939bdfe6a2d7e70720dca8",
     "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "672e6d5974ed05010533442279ac8703",
+    "url": "statics/icons/icon-192x192.png"
   },
   {
     "revision": "15327de252d4457541d192e2b8b62977",
@@ -56,16 +56,16 @@ self.__precacheManifest = [
     "url": "statics/icons/favicon-16x16.png"
   },
   {
-    "revision": "a1471d1d6431c893582a5f6a250db3f9",
-    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.a1471d1d.woff"
+    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.e9dbbe8a.woff"
   },
   {
-    "revision": "6ef31ad29c0873080f288d318a6c24e2",
+    "revision": "4c23f12e9fedc1db4e8f771fa55e71b5",
     "url": "index.html"
   },
   {
-    "revision": "e9dbbe8a693dd275c16d32feb101f1c1",
-    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.e9dbbe8a.woff"
+    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.bafb105b.woff"
   },
   {
     "revision": "8a9a261c8b8dfe90db11f1817a9d22e1",
@@ -84,23 +84,23 @@ self.__precacheManifest = [
     "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.c38ebd3c.woff"
   },
   {
-    "revision": "bafb105baeb22d965c70fe52ba6b49d9",
-    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.bafb105b.woff"
+    "revision": "cf6613d1adf490972c557a8e318e0868",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.cf6613d1.woff"
   },
   {
-    "revision": "8c2ade503b34e31430d6c98aa29a52a3",
-    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.8c2ade50.woff"
+    "revision": "a1471d1d6431c893582a5f6a250db3f9",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.a1471d1d.woff"
   },
   {
     "revision": "de8b7431b74642e830af4d4f4b513ec9",
     "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.de8b7431.woff"
   },
   {
-    "revision": "804957339718e5125147",
+    "revision": "4411d3746195abe845ae",
     "url": "css/app.99127b2f.css"
   },
   {
-    "revision": "f530119605145068262e",
+    "revision": "c4903bb35a3504159d00",
     "url": "css/92367fdc.7ac28700.css"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.6ec84c634918280524923b8ceef6d730.js"
+  "precache-manifest.5586bbd4bcb8565fbc831ba944c6bfa8.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "mqtt-board"});
