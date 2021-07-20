53ed47e4daf5579bc500a9e9c373dd8743540555
Siarhei Buntsevich <scarry92@yandex.ru>
2021-07-20 17:37:16 +0300

    publish: v.2.6.1 [feature] auto expand tree to valued leaf
    generated from commit d59a469e39a188f514dde49393f3fccba0166ac0

--- js/6.e0252515.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([[6],{f241:function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("q-layout",{attrs:{view:"lHh Lpr lFf"}},[n("q-page-container",[n("router-view")],1)],1)},r=[],l={name:"MyLayout"},u=l,o=n("2877"),c=Object(o["a"])(u,a,r,!1,null,null,null);t["default"]=c.exports}}]);
